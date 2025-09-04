fe5243b6fbea19c5c1a9d1d0ce0e0217897632a9
dutchLuck <dutchLuck@live.com>
2025-09-04 18:58:58 +1000

    Added min, median, max and range to stats calculations

--- bfbs.pl
+++ bfbs.pl
@@ -2,7 +2,7 @@
 #
 # B F B S . P L
 #
-# bfbs.pl last edited on Wed Sep  3 22:12:34 2025
+# bfbs.pl last edited on Wed Sep  3 22:29:34 2025
 #
 # This script reads one or more CSV files, containing one or more columns of
 # numbers and calculates basic statistics for each column (including sum,
@@ -16,19 +16,21 @@ use Math::BigFloat;
 
 # ChatGPT prompts that produced thid bfbs.pl script; -
 # 1. Please write a perl script that reads one or more 
-# comma separated value data files containing one or more
-# columns of numbers and calculates the sum, mean,
-# variance and standard deviation for each column
-# using the arbitrary-precision floating point
-# arithmetic package Math::BigFloat and writes
-# out the results for each column in each file.
-# Ensure that the script can handle leading spaces
-# on numbers, blank rows in files and comment lines
-# beginning with a hash. Allow the precision to be
-# controlled by the user from the command line.
+#    comma separated value data files containing one or more
+#    columns of numbers and calculates the sum, mean,
+#    variance and standard deviation for each column
+#    using the arbitrary-precision floating point
+#    arithmetic package Math::BigFloat and writes
+#    out the results for each column in each file.
+#    Ensure that the script can handle leading spaces
+#    on numbers, blank rows in files and comment lines
+#    beginning with a hash. Allow the precision to be
+#    controlled by the user from the command line.
 # 2. Please make the calculated variance the sample form
-# of variance i.e. = sum((x - mean)^2) / (n - 1)
-# unless the user sets an option in the command line.
+#    of variance i.e. = sum((x - mean)^2) / (n - 1)
+#    unless the user sets an option in the command line.
+# 3. Please enhance the script to calculate and output
+#    min, median, max and range.
 
 # Default settings
 my $precision = 40;
@@ -40,7 +42,7 @@ GetOptions(
 ) or die "Usage: $0 [--precision=N] [--population] file1.csv file2.csv ...\n";
 
 # Set global Math::BigFloat precision
-Math::BigFloat->precision(-$precision);  # Negative means significant digits
+Math::BigFloat->precision(-$precision);  # Negative => significant digits
 
 # Require at least one file
 @ARGV or die "Usage: $0 [--precision=N] [--population] file1.csv file2.csv ...\n";
@@ -82,7 +84,7 @@ foreach my $file (@ARGV) {
 
         my $mean = $sum->copy()->bdiv($n);
 
-        # Variance calculation
+        # Variance
         my $var_sum = Math::BigFloat->new(0);
         for my $x (@$data) {
             my $diff = $x->copy()->bsub($mean);
@@ -101,10 +103,31 @@ foreach my $file (@ARGV) {
         }
 
         my $variance = $var_sum->copy()->bdiv($divisor);
-        my $stddev = $variance->copy()->bsqrt();
+        my $stddev   = $variance->copy()->bsqrt();
+
+        # Sort data for min, max, median
+        my @sorted = sort { $a->bcmp($b) } @$data;
+        my $min    = $sorted[0]->copy();
+        my $max    = $sorted[-1]->copy();
+        my $range  = $max->copy()->bsub($min);
+        my $median;
+
+        if ($n % 2 == 1) {
+            # Odd number of elements
+            $median = $sorted[int($n / 2)]->copy();
+        } else {
+            # Even number of elements: average of middle two
+            my $mid1 = $sorted[$n/2 - 1]->copy();
+            my $mid2 = $sorted[$n/2]->copy();
+            $median = $mid1->badd($mid2)->bdiv(2);
+        }
 
         print "\nColumn ", $i + 1, ":\n";
         print "  Count     : $n\n";
+        print "  Min       : $min\n";
+        print "  Median    : $median\n";
+        print "  Max       : $max\n";
+        print "  Range     : $range\n";
         print "  Sum       : $sum\n";
         print "  Mean      : $mean\n";
         print "  Variance  : $variance ", ($use_population ? "(population)" : "(sample)"), "\n";
